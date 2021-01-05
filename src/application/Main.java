52a819c5510c811323d9bfe33d7a5fdcc481aa5d
Brianna Irie <birie1@my.westga.edu>
2021-01-04 23:59:35 -0500

    Started setting up application gui

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,12 @@
 package application;
 
-import application.datatier.JsonFileWriter;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * The Class Main - the entry point for the application
@@ -9,15 +15,33 @@ import application.datatier.JsonFileWriter;
  * @version August 2020
  *
  */
-public class Main {
+public class Main extends Application {
+	private static final String GUI_FXML = "view/ApplicationGui.fxml";
+	private static final String WINDOW_TITLE = "Excel to Json";
+
 	/**
 	 * Launches application
 	 *
 	 * @param args not used
 	 */
 	public static void main(String[] args) {
-		JsonFileWriter writer = new JsonFileWriter();
-		writer.createJsonFile();
+		launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource(GUI_FXML));
+			Pane pane = loader.load();
+			Scene scene = new Scene(pane);
+			stage.setScene(scene);
+			stage.setTitle(WINDOW_TITLE);
+			stage.show();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 		
 	}
 }
